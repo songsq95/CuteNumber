f3617b3dec5b6e379944e2d77ee0fd0985ddc5d1
Siqi Song <ssong6@slb.com>
2022-10-25 17:07:00 +0800

    feat: complete isCuteNumberGreaterThan10

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,20 @@ export class Foo {
     return 'bar'
   }
 }
+
+export function isCuteNumberGreaterThan10(numbers: number[]): boolean {
+  const evenNumbers = findAllEvenNumbers(numbers); 
+  //console.log(evenNumbers);
+  const max = findTheMax(evenNumbers);
+  //console.log(max);
+  return max>10;
+}
+
+export function findAllEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter(number=> number%2 ===0);
+}
+
+export function findTheMax(numbers: number[]): number {
+  return Math.max(...numbers);
+}
+
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,19 @@
-import {Foo} from '../src/Foo'
+import {Foo, isCuteNumberGreaterThan10} from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
+
+  it('should return true given numbers: 1, 5, 6, 10, 11, 14',()=>{
+    //given
+    const numbers=[1, 5, 6, 10, 11, 14];
+    //when
+    const actual:boolean = isCuteNumberGreaterThan10(numbers);
+    //then
+    expect(actual).toBeTruthy();
+  });
+  
 })
+
+
